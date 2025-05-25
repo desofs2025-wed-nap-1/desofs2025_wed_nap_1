9d15707f4ba6202f1882d3c7379f3783db5d7a93
1181031 <1181031@isep.ipp.pt>
2025-05-25 22:13:10 +0100

    fix crud

--- ParkingSystem/ParkingSystem/ApplicationDbContext.cs
+++ ParkingSystem/ParkingSystem/ApplicationDbContext.cs
@@ -9,9 +9,9 @@ namespace ParkingSystem.Infrastructure.Data
         {
         }
 
-        public DbSet<User> Users { get; set; }
-        public DbSet<Vehicle> Vehicles { get; set; }
-        public DbSet<Park> Parks { get; set; }
+        public DbSet<User> users { get; set; }
+        public DbSet<Vehicle> vehicle { get; set; }
+        public DbSet<Park> park { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
